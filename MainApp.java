3928a4a930c2eb3b5ae5db1d641e6c54d119a8f5
Nguyen Van Quyen <153894626+quyen0505@users.noreply.github.com>
2023-12-15 14:58:51 +0700

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,23 @@
+package basic.dev;
+/**
+ * Nguyen Van Quyen
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = " Nguyen Van ";
+		String lastName = "Quyen ";
+		int old = 18;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println(" Ho Va Ten : ");
+		System.out.println(firstName + "" +lastName);
+		System.out.println(" Mark Math : " + markMath);
+		System.out.println(" Passed : " + passed);
+		final float PI = 3.14f;
+		
+	}
+
+}
